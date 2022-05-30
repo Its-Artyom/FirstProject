a098224bcd732270d5fedc0522f32f92d7db0def
Artyom P <pionovartem11@gmail.com>
2022-05-30 10:45:04 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        String name = "David";
+        int age = 16;
+        System.out.println(name + " is " + age + " years old.");
+    }
+}

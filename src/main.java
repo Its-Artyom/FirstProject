6ca96ded90aa4cf5762197ad50e4e3d7c74dc575
Artyom P <pionovartem11@gmail.com>
2022-05-30 11:41:51 +0100

    New changes

--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-import java.util.Locale;
+import java.util.Scanner;
 
 /* Purpose
 First project is where I will learn different things is java :)
@@ -11,22 +11,21 @@ public class main {
 
         Multiple variable of same type
         type name1 = value1, name2 = value2, etc...
-
-        Java Type Casting
-        Type casting is when you assign a value of one primitive data type to another type.
-
-        In Java, there are two types of casting:
-
-        Widening Casting (automatically) - converting a smaller type to a larger type size
-        byte -> short -> char -> int -> long -> float -> double
-
-        Narrowing Casting (manually) - converting a larger type to a smaller size type
-        double -> float -> long -> int -> char -> short -> byte
         */
 
         String welcoming = "Hello World!";
         System.out.println(welcoming); // This Is A Cool Phrase!
         System.out.println(welcoming.toLowerCase()); // this is a cool phrase!
         System.out.println(welcoming.toUpperCase()); // THIS IS A COOL PHRASE!
+
+        // Concatenation
+        // First set up the input system.
+        Scanner input = new Scanner(System.in);
+        // Get inputs
+        System.out.println("Enter first name: ");
+        String f_name = input.next();
+        System.out.println("Enter last name: ");
+        String l_name = input.next();
+        System.out.println(f_name + " ".concat(l_name));
     }
 }

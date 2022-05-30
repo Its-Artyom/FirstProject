c86eca4d18d859252dee9a38af22a71c85241ea0
Artyom P <pionovartem11@gmail.com>
2022-05-30 10:52:55 +0100

    New changes

--- src/main.java
+++ src/main.java
@@ -1,7 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        String name = "David";
-        int age = 16;
-        System.out.println(name + " is " + age + " years old.");
+        System.out.println("Hello World");
     }
 }
